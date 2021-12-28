c5694b9392d35052865afc59d0ab72001a35999f
Eduard Eisenholz <82213560+eduardeisenholz@users.noreply.github.com>
2021-12-28 15:19:25 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,26 +1,32 @@
 exports.handleWebhookEvents = (req, res) => {
   switch (req.method) {
     case 'GET': {
-      const path = req.path || '/'
-      const query = req.query || {}
-      if (path === '/app') {
-        res.status(200).send('<h1>The Coyoyte</h1>')
-      } else if (query['hub.verify_token'] === process.env.VERIFY_TOKEN) {
-        res.status(200).send({
-          'hub.challenge': query['hub.challenge']
-        })
+      if (req.query['hub.mode'] === 'subscribe') {
+        if (req.query['hub.verify_token'] === process.env.VERIFY_TOKEN) {
+          res.status(200).send({
+            'hub.challenge': req.query['hub.challenge']
+          })
+        } else {
+          res.status(403).send({
+            statusCode: 403,
+            statusMessage: 'Forbidden'
+          })
+        }
+      } else {
+        res.status(200).send('<h1>Strava Webhook Application</h1>')
+      }
+      break
+    }
+    case 'POST': {
+      if (req.body.subscription_id === process.env.SUBSCRIPTION_ID) {
+         console.log('EVENT_RECEIVED: %s', JSON.stringify(req.body))
+         res.status(200).end()
       } else {
         res.status(403).send({
           statusCode: 403,
           statusMessage: 'Forbidden'
         })
       }
-      break
-    }
-    case 'POST': {
-      const subscription_id = req.body.subscription_id
-      console.log('Event', req.body)
-      res.status(200).end()
       break
     }
     default:
